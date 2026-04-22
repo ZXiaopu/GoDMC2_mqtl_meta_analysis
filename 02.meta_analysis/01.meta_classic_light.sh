1fc0e4559d7aee6e5abb08cbf1ad17d3b06e39ae
xiaopuz b35cx <xiaopuz.b35cx@login01.head.cm.i3.isambard.ac.uk>
2026-04-22 11:05:25 +0000

    update variables

--- 02.meta_analysis/01.meta_classic_light.sh
+++ 02.meta_analysis/01.meta_classic_light.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -l
-#SBATCH --output=meta_classic_%j.out
-#SBATCH --job-name=meta-classic
-#SBATCH --nodes=2
-#SBATCH --time=24:0:0
 
-conda activate hase_py2
-
-source config
+source ../config
 
 genotype_input=$1
 study_name_input=$2
@@ -15,12 +9,11 @@ derivatives_input=$4
 snp_inc_input=$5
 cpg_inc_input=$6
 encoded_input=$7
-node=$8
+covariate_selection=$8
+node=$9
+meta_analysis_output=${10}
 
-cohort1="/scratch/prj/bell/recovered/epigenetics/Analysis/subprojects/xiaopu/GoDMC/GoDMC_TwinsUK_EPIC/godmc_phase2/results/04"
-cohort2="/scratch/prj/bell/recovered/epigenetics/Analysis/subprojects/xiaopu/GoDMC/Stage2/meQTL_results/Cluster_Filespace/Marioni_Group/Xiaopu/godmc_unrelated/godmc_phase2/results/04"
-
-python ${hase}/hase.py \
+apptainer exec -B /projects ${project_path}/hase_glint_py2_isb3.sif python2 ${hase}/hase.py \
 -g ${genotype_input} \
 -study_name ${study_name_input} \
 -ph ${phenotype_input} \
@@ -38,6 +31,6 @@ python ${hase}/hase.py \
 -mode meta-classic \
 -max-missingness-rate 1 \
 -cluster "y" \
--node 10 ${node}
+-node ${node}
 
 echo "Running meta-classic mode has been done"
